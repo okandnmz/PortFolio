3bf201034653f978d052a0e7889500cdf8b22d2a
okandnmz <166050035+okandnmz@users.noreply.github.com>
2024-04-15 14:40:26 +0200

    Update main.js

--- main.js
+++ main.js
@@ -260,7 +260,7 @@
   new PureCounter();
 
   document.getElementById("cv-btn").addEventListener("click", function() {
-    window.open('', '_blank');
+    window.open('CV DONMEZ Okan.pdf', '_blank');
 });
 
 
